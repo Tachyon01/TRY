855e437a028b2974ed4fbb2b1316c5b8203c7439
Pulkit Goel <pulkitgoel234@gmail.com>
2019-01-27 18:50:33 +0530

    Publish

---
+++ publish.ino
@@ -0,0 +1,86 @@
+//Header Files to be used
+#include<ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+//Adafruit.io details;
+#define AIO_SERVER      "http://username:password@172.31.1.4:8080/io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME  "Tachyon01"
+#define AIO_KEY  "19d6e75a354a423a8a64d198eef9cc76"
+
+WiFiClient client;
+
+//To connect to Adafruit server
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish Recieve = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Recieve");
+
+
+String x;
+
+void Wifi()           //Connect to Wifi
+{
+  WiFi.disconnect();
+  delay(1000);
+  Serial.println("START");
+  WiFi.begin("Ni milega","bepa8139");     //Change to your credentials
+  while(!(WiFi.status() == WL_CONNECTED))
+  {
+    delay(300);
+    Serial.print("...");
+  }
+  Serial.println("Connected");
+  Serial.println("Your IP is ");
+  Serial.print(WiFi.localIP().toString());
+}
+
+void MQTT_connect()
+{
+    if (mqtt.connected())
+    {
+      return;
+    }
+    int8_t ret;
+    Serial.println("Coneecting to MQTT");
+    uint8_t retries = 3;
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");    
+}
+
+void setup() 
+{
+  Serial.begin(9600);
+  Wifi();     //Connect to Wifi
+  pinMode(D4,OUTPUT);        
+  mqtt.subscribe(&led);     //Subscribe to Light feed to control colour
+  MQTT_connect();             //Connect to MQTT server on of adafruit
+}
+
+
+
+void loop()
+{
+  if(mqtt.connected()==0)     //If mqtt connection failed, reconnect
+  {
+    MQTT_connect();
+  }
+  if (MQTT_connect()) {
+      if (HI.publish("data")) {
+        Serial.println("done");
+
+      } else 
+      {
+        Serial.println("fail");
+      }
+    }
+}
