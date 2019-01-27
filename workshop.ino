13a441d656ba3d111e8279335696db924dacc8b2
Pulkit Goel <pulkitgoel234@gmail.com>
2019-01-27 16:27:59 +0530

    Update workshop.ino

--- workshop.ino
+++ workshop.ino
@@ -11,18 +11,11 @@
 
 WiFiClient client;
 
-////To connect to IFTTT
-//String MakerIFTTT_Key ;
-//String MakerIFTTT_Event;
-//char *append_str(char *here, String s) {  int i=0; while (*here++ = s[i]){i++;};return here-1;}
-//char *append_ul(char *here, unsigned long u) { char buf[20]; return append_str(here, ultoa(u, buf, 10));}
-//char post_rqst[256];char *p;char *content_length_here;char *json_start;int compi;
-
 //To connect to Adafruit server
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 Adafruit_MQTT_Subscribe led = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/led");
 
-char* x;
+String x;
 
 void Wifi()           //Connect to Wifi
 {
@@ -46,10 +39,10 @@ void MQTT_connect()
     {
       return;
     }
-int8_t ret;
+    int8_t ret;
     Serial.println("Coneecting to MQTT");
-     uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    uint8_t retries = 3;
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
        Serial.println("Retrying MQTT connection in 5 seconds...");
        mqtt.disconnect();
@@ -67,10 +60,9 @@ void setup()
 {
   Serial.begin(9600);
   Wifi();     //Connect to Wifi
-//  pinMode(Bell,INPUT);        //To read pressing of bell
-//  t=millis();                 //Get current time for board 
-    mqtt.subscribe(&led);     //Subscribe to Light feed to control colour
-    MQTT_connect();             //Connect to MQTT server on of adafruit
+  pinMode(D4,OUTPUT);        
+  mqtt.subscribe(&led);     //Subscribe to Light feed to control colour
+  MQTT_connect();             //Connect to MQTT server on of adafruit
 }
 
 
@@ -82,24 +74,15 @@ void loop()
     MQTT_connect();
   }
   Adafruit_MQTT_Subscribe *subscription_name;
-while (subscription_name = mqtt.readSubscription(5000)){
-  x=((char *)led.lastread);
+  while (subscription_name = mqtt.readSubscription(5000)){
+  x=((char *  )led.lastread);
   Serial.println(x);
-  if(strcmp(x,"OF"))
+  if(x[1]=='F')
   {
-  digitalWrite(BUILTIN_LED,HIGH);
+  digitalWrite(D4,HIGH);
   }
   else
   {
-    digitalWrite(BUILTIN_LED,LOW);
+    digitalWrite(D4,LOW);
   }}
-  }
-// 
-//  if(digitalRead(Bell)==0 && b_on+2000<millis())  //Read if bell pressed and time between two press is atleast 2 sec
-//  {
-//     b_on=millis();                               //Update time when bell was pressed
-//     Serial.println("Bell pressed");
-//     Bell_notification();                          //Call function to send notification
-//  }
-  
-
+}
